aa81125f8b545e3b6f3a5f00e58c8bbe1a8b872b
Jürgen Müller <mueller@mueller-se.de>
2020-06-05 10:38:21 +0200

    all supported currencies added

--- EzbAdapter/EzbAdapter/Contracts/Currency.cs
+++ EzbAdapter/EzbAdapter/Contracts/Currency.cs
@@ -1,13 +1,25 @@
 ﻿namespace EzbAdapter.Contracts
 {
-    public enum Currency
-    {
+	/// <summary>
+	/// Complete list: https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html
+	/// </summary>
+	public enum Currency
+	{
 		USD, GBP,
 		JPY, CHF,
 		CAD, AUD,
 		NZD, ZAR,
-		HKD, SEK, 
+		HKD, SEK,
 		MXN, RUB,
-		NOK
+		NOK, BGN,
+		CZK, DKK,
+		HUF, PLN,
+		RON, ISK,
+		HRK, TRY,
+		BRL, CNY,
+		IDR, ILS,
+		INR, KRW,
+		MYR, PHP,
+		SGD, THB
 	}
 }
